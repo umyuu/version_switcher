44864f05cf5be6a7f7d6922b6b566aa942c0498a
umyuu <124dtiaka@gmail.com>
2018-05-10 10:22:59 +0900

    change templete

--- src/js/version_switcher.js
+++ src/js/version_switcher.js
@@ -1,50 +1,27 @@
-﻿//@version_switcher.js
+﻿//@file version_switcher.js
 (function()
 {
     'use strict';
     const VERSION_STRINGS = /([\d.]+)\/(docs\/)?api/;
     let switcher = undefined;
     let browser = chrome;
-    let template_html = undefined;
-
-    class HtmlUtils {
-        /**
-         * @param {Map<string, string>} options
-         * @param {string} selected
-        */
-        static create_selectBox(options, selected){
-            const selectbox = document.createElement('select');
-            // Todo:順序性は保証していない。
-            for(let [key, value] of options){
-                const opt = document.createElement('option');
-                opt.text = key;
-                opt.value = value;
-                if(opt.text === selected){
-                    opt.selected = true;
-                }
-                selectbox.appendChild(opt);
-            }
-            return selectbox;
-        }
-    }
     class Switcher {
         constructor() {
-            this.docs = undefined;
-            this.docs = new Map([["10", "10/1"], ["9", "9/1"], ["8", "8/1"]
-            ,["7", "7/0"], ["6", "6/0"], ["1.5.0", "1.5.0/0"]]);
             this.url = new URL(window.location.href);
             this.version = VERSION_STRINGS.exec(this.url.pathname);
             Object.seal(this);
         }
         generate() {
-
             (async() => {
                 const response = await fetch(browser.extension.getURL("resources/template.html"));
                 const nav = document.createElement('div');
                 nav.innerHTML = await response.text();
                 const select_box = nav.children[0].children[0];
-                Array.from(select_box.options).filter(x => x.text === this.version[1])[0].selected = true;
-
+                // セレクトボックスの初期値を選択
+                const opts = Array.from(select_box.options).filter(x => x.text === this.version[1]);
+                if (opts.length !== 0){
+                    opts[0].selected = true;
+                }
                 select_box.addEventListener('change', (e) => {
                     this.on_switch(e);
                 }, false);
@@ -59,33 +36,6 @@
                     header.after(nav);
                 }
             })();
-
-
-            // セレクトボックスを作成して、hrefのバージョン情報より初期選択を行う。
-            //const select_box = HtmlUtils.create_selectBox(this.docs, this.version[1]);
-            //const nav = document.createElement("nav");
-            //nav.className = "version_switcher";
-            //nav.style = "";
-            //nav.appendChild(document.createTextNode(" Java » "));
-            //nav.appendChild(select_box);
-            //nav.appendChild(document.createTextNode("ドキュメント"));
-            //nav.appendChild(document.createElement("hr"));
-            
-            //let header = document.querySelector("ul.navList");
-            //if (header === null){
-                // Java 9 以前
-            //    document.body.prepend(nav);
-            //}else{
-            //    header.after(nav);
-            //}
-            //
-           // select_box.addEventListener('change', (e) => {
-           //     this.on_switch(e);
-           // }, false);
-           // window.addEventListener('scroll', (e) =>{
-           //     //nav.setAttribute('data-fixed', e.currentTarget.pageYOffset > 50 ? '1' : '0');
-           //     nav.setAttribute('display', e.currentTarget.pageYOffset > 50 ? 'none' : 'block');
-           //   }, false);
             return this;
         }
         /**
@@ -141,6 +91,5 @@
     }
     window.addEventListener('DOMContentLoaded', (e) => {
         switcher = new Switcher().generate();
-        window.switcher = switcher;
     }, false);
 })();
